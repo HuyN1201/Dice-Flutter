27130d0c283ddad50e5b48d78599d96548369d8b
HuyN1201 <huyn120198@gmail.com>
2020-10-01 17:32:01 -0500

    Added the rest of the code

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=C:\src\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\huyng\OneDrive\Documents\Flutter Development Bootcamp\dicee-flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Users\Huy Nguyen\Documents\FlutterDevelopment\Dice-Flutter"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build\ios"
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   return runApp(
@@ -15,7 +16,19 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int ldiceNum = 1;
+  int rdiceNum = 1;
+  void changeDice() {
+    ldiceNum = Random().nextInt(6) + 1;
+    rdiceNum = Random().nextInt(6) + 1;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -23,10 +36,24 @@ class DicePage extends StatelessWidget {
         child: Row(
           children: [
             Expanded(
-              child: Image.asset('images/dice1.png'),
+              child: FlatButton(
+                onPressed: () {
+                  setState(() {
+                    changeDice();
+                  });
+                },
+                child: Image.asset('images/dice$ldiceNum.png'),
+              ),
             ),
             Expanded(
-              child: Image.asset('images/dice6.png')
+              child: FlatButton(
+                onPressed: () {
+                  setState(() {
+                    changeDice();
+                  });
+                },
+                child: Image.asset('images/dice$rdiceNum.png'),
+              ),
             ),
           ],
         ),
@@ -34,3 +61,26 @@ class DicePage extends StatelessWidget {
     );
   }
 }
+
+//This stateless widget will not dynamically update
+// class DicePage extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Container(
+//       child: Center(
+//         child: Row(
+//           children: [
+//             Expanded(
+//               child: FlatButton(
+//                   child: Image.asset('images/dice1.png')),
+//             ),
+//             Expanded(
+//                 child: FlatButton(
+//                     child: Image.asset('images/dice6.png'))
+//             ),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
